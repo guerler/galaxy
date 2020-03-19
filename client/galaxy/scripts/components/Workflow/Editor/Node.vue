f58364de514bfd786fa8531c22f0c82449306a09
guerler <aysam.guerler@gmail.com>
2020-03-18 23:32:41 -0400

    Consolidate three workflow node icons into one

--- client/galaxy/scripts/components/Workflow/Editor/Node.vue
+++ client/galaxy/scripts/components/Workflow/Editor/Node.vue
@@ -5,23 +5,29 @@
                 class="node-destroy py-0 float-right"
                 variant="primary"
                 size="sm"
-                aria-label="clone node"
+                aria-label="destroy node"
                 @click="onDestroy"
             >
                 <i class="fa fa-times" />
             </b-button>
-            <b-button
-                v-if="canClone"
-                class="node-clone py-0 float-right"
+            <b-dropdown
+                right
+                no-caret
+                class="node-dropdown float-right"
+                toggle-class="py-0 px-1"
+                offset="50"
                 variant="primary"
                 size="sm"
-                aria-label="clone node"
-                @click="onClone"
             >
-                <i class="fa fa-files-o" />
-            </b-button>
-            <i :class="iconClass" />
-            <span class="node-title">{{ title }}</span>
+                <template v-slot:button-content>
+                    <i class="fa fa-fw fa-caret-down" />
+                </template>
+                <b-dropdown-item aria-label="clone node" role="button" href="#" class="node-clone" @click="onClone">
+                    <span class="fa fa-fw fa-files-o" />
+                    Duplicate
+                </b-dropdown-item>
+            </b-dropdown>
+            <span class="node-title ml-1">{{ title }}</span>
         </div>
         <div class="node-body">
             <div>
@@ -34,7 +40,6 @@
 <script>
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
-import WorkflowIcons from "components/Workflow/icons";
 import LoadingSpan from "components/LoadingSpan";
 
 Vue.use(BootstrapVue);
@@ -59,18 +64,6 @@ export default {
         node: {
             type: Object,
             default: null,
-        },
-    },
-    computed: {
-        iconClass() {
-            const iconType = WorkflowIcons[this.type];
-            if (iconType) {
-                return `icon fa fa-fw ${iconType}`;
-            }
-            return null;
-        },
-        canClone() {
-            return this.type != "subworkflow";
         },
     },
     methods: {

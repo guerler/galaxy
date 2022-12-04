35ff334075513db4b978fac33ef0d9c2fbaa26ba
guerler <aysam.guerler@gmail.com>
2022-12-03 21:35:59 -0500

    Add navitem to sidebar

--- client/src/entry/analysis/modules/Analysis.vue
+++ client/src/entry/analysis/modules/Analysis.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { getGalaxyInstance } from "app";
+import CenterFrame from "./CenterFrame";
 import HistoryIndex from "components/History/Index";
+import MastheadItem from "components/Masthead/MastheadItem";
 import ToolBox from "components/Panels/ProviderAwareToolBox";
 import SidePanel from "components/Panels/SidePanel";
-import CenterFrame from "./CenterFrame";
 import { useRoute, useRouter } from "vue-router/composables";
 import { computed, ref, onMounted, onUnmounted } from "vue";
 
 const route = useRoute();
 const router = useRouter();
 const showCenter = ref(false);
+const searchTab = ref({ id: "tool-search", icon: "fa-search" });
+const searchToggle = ref(false);
 
 // computed
 const showPanels = computed(() => {
@@ -33,6 +36,11 @@ function hideCenter() {
 function onLoad() {
     showCenter.value = true;
 }
+function onSearchToggle() {
+    console.log("here");
+    console.log(searchToggle.value);
+    searchToggle.value = !searchToggle.value;
+}
 
 // life cycle
 onMounted(() => {
@@ -46,8 +54,16 @@ onUnmounted(() => {
 </script>
 <template>
     <div id="columns" class="d-flex">
-        <div style="side-bar">Looks ok</div>
-        <ToolBox v-bind="toolBoxProperties" class="side-column" />
+        <div style="side-bar">
+            <b-nav-item @click="onSearchToggle">
+                <template>
+                    <span class="fa fa-search" />
+                </template>
+            </b-nav-item>
+        </div>
+        <div v-show="searchToggle">
+            <ToolBox v-bind="toolBoxProperties" class="side-column" />
+        </div>
         <div class="center-column overflow-auto p-3 w-100">
             <CenterFrame v-show="showCenter" id="galaxy_main" @load="onLoad" />
             <router-view v-show="!showCenter" class="h-100" :key="$route.fullPath" />
@@ -56,6 +72,11 @@ onUnmounted(() => {
     </div>
 </template>
 <style scoped>
+.nav-item {
+    cursor: pointer;
+    text-decoration: none;
+    list-style-type: none;
+}
 .side-bar {
     width: 3rem;
     min-width: 3rem;

db1cbc07367d5a0dd48878622eee078fc3639016
guerler <aysam.guerler@gmail.com>
2022-12-03 21:05:03 -0500

    Convert analysis module to composition api

--- client/src/entry/analysis/modules/Analysis.vue
+++ client/src/entry/analysis/modules/Analysis.vue
@@ -1,5 +1,52 @@
+<script setup>
+import { getGalaxyInstance } from "app";
+import HistoryIndex from "components/History/Index";
+import ToolBox from "components/Panels/ProviderAwareToolBox";
+import SidePanel from "components/Panels/SidePanel";
+import CenterFrame from "./CenterFrame";
+import { useRoute, useRouter } from "vue-router/composables";
+import { computed, ref, onMounted, onUnmounted } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+const showCenter = ref(false);
+
+// computed
+const showPanels = computed(() => {
+    const panels = route.query.hide_panels;
+    if (panels !== undefined) {
+        return panels.toLowerCase() != "true";
+    }
+    return true;
+});
+const toolBoxProperties = computed(() => {
+    const Galaxy = getGalaxyInstance();
+    return {
+        storedWorkflowMenuEntries: Galaxy.config.stored_workflow_menu_entries,
+    };
+});
+
+// methods
+function hideCenter() {
+    showCenter.value = false;
+}
+function onLoad() {
+    showCenter.value = true;
+}
+
+// life cycle
+onMounted(() => {
+    // Using a custom event here which, in contrast to watching $route,
+    // always fires when a route is pushed instead of validating it first.
+    router.app.$on("router-push", hideCenter);
+});
+onUnmounted(() => {
+    router.app.$off("router-push", hideCenter);
+});
+</script>
 <template>
     <div id="columns" class="d-flex">
+        <div style="side-bar">Looks ok</div>
         <ToolBox v-bind="toolBoxProperties" class="side-column" />
         <div class="center-column overflow-auto p-3 w-100">
             <CenterFrame v-show="showCenter" id="galaxy_main" @load="onLoad" />
@@ -8,59 +55,12 @@
         <HistoryIndex class="side-column" />
     </div>
 </template>
-<script>
-import { getGalaxyInstance } from "app";
-import HistoryIndex from "components/History/Index";
-import ToolBox from "components/Panels/ProviderAwareToolBox";
-import SidePanel from "components/Panels/SidePanel";
-import CenterFrame from "./CenterFrame";
-
-export default {
-    components: {
-        CenterFrame,
-        SidePanel,
-        ToolBox,
-        HistoryIndex,
-    },
-    data() {
-        return {
-            showCenter: false,
-        };
-    },
-    computed: {
-        showPanels() {
-            const panels = this.$route.query.hide_panels;
-            if (panels !== undefined) {
-                return panels.toLowerCase() != "true";
-            }
-            return true;
-        },
-        toolBoxProperties() {
-            const Galaxy = getGalaxyInstance();
-            return {
-                storedWorkflowMenuEntries: Galaxy.config.stored_workflow_menu_entries,
-            };
-        },
-    },
-    mounted() {
-        // Using a custom event here which, in contrast to watching $route,
-        // always fires when a route is pushed instead of validating it first.
-        this.$router.app.$on("router-push", this.hideCenter);
-    },
-    beforeDestroy() {
-        this.$router.app.$off("router-push", this.hideCenter);
-    },
-    methods: {
-        hideCenter() {
-            this.showCenter = false;
-        },
-        onLoad() {
-            this.showCenter = true;
-        },
-    },
-};
-</script>
 <style scoped>
+.side-bar {
+    width: 3rem;
+    min-width: 3rem;
+    max-width: 3rem;
+}
 .side-column {
     min-width: 18rem;
     max-width: 18rem;

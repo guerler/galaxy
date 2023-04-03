9d5109961564f8b81c14c1df1608e14c98759c68
tcollins <tyler@nhlp.com>
2023-04-03 13:45:38 -0600

    removed did you mean prompt from this branch

--- client/src/components/Panels/ToolBox.vue
+++ client/src/components/Panels/ToolBox.vue
@@ -43,9 +43,6 @@
                 <div v-else-if="queryFinished" class="pb-2">
                     <b-badge class="alert-danger w-100">No results found!</b-badge>
                 </div>
-                <div v-else-if="misspelled" class="pb-2">
-                    <b-badge class="alert-danger w-100">You made a typo!</b-badge>
-                </div>
             </section>
         </div>
         <div v-if="!showAdvanced" class="unified-panel-body">
@@ -133,9 +130,6 @@ export default {
         },
         queryFinished() {
             return this.query && this.queryPending != true;
-        },
-        misspelled(){
-            return this.query && this.query.length >= 2;
         },
         sections() {
             if (this.showSections) {

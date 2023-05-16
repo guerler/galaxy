41f7e9b99b5ad32281aa52dac07cfe1caf0ab0f8
guerler <aysam.guerler@gmail.com>
2023-05-16 10:08:04 +0300

    Move data selector configuration to form data utilities

---
+++ client/src/components/Form/Elements/FormData/utilities.ts
@@ -0,0 +1,114 @@
+/** Batch mode variations */
+const Batch = { DISABLED: "disabled", ENABLED: "enabled", LINKED: "linked" };
+
+/** List of available content selectors options */
+export const Configurations = {
+    data: [
+        {
+            src: "hda",
+            icon: "fa-file-o",
+            tooltip: "Single dataset",
+            library: true,
+            multiple: false,
+            batch: Batch.DISABLED,
+        },
+        {
+            src: "hda",
+            icon: "fa-files-o",
+            tooltip: "Multiple datasets",
+            multiple: true,
+            batch: Batch.LINKED,
+        },
+        {
+            src: "hdca",
+            icon: "fa-folder-o",
+            tooltip: "Dataset collection",
+            multiple: false,
+            batch: Batch.LINKED,
+        },
+    ],
+    data_multiple: [
+        {
+            src: "hda",
+            icon: "fa-files-o",
+            tooltip: "Multiple datasets",
+            multiple: true,
+            batch: Batch.DISABLED,
+        },
+        {
+            src: "hdca",
+            icon: "fa-folder-o",
+            tooltip: "Dataset collections",
+            multiple: true,
+            batch: Batch.DISABLED,
+        },
+    ],
+    data_collection: [
+        {
+            src: "hdca",
+            icon: "fa-folder-o",
+            tooltip: "Dataset collection",
+            multiple: false,
+            batch: Batch.DISABLED,
+        },
+    ],
+    workflow_data: [
+        {
+            src: "hda",
+            icon: "fa-file-o",
+            tooltip: "Single dataset",
+            multiple: false,
+            batch: Batch.DISABLED,
+        },
+    ],
+    workflow_data_multiple: [
+        {
+            src: "hda",
+            icon: "fa-files-o",
+            tooltip: "Multiple datasets",
+            multiple: true,
+            batch: Batch.DISABLED,
+        },
+    ],
+    workflow_data_collection: [
+        {
+            src: "hdca",
+            icon: "fa-folder-o",
+            tooltip: "Dataset collection",
+            multiple: false,
+            batch: Batch.DISABLED,
+        },
+    ],
+    module_data: [
+        {
+            src: "hda",
+            icon: "fa-file-o",
+            tooltip: "Single dataset",
+            multiple: false,
+            batch: Batch.DISABLED,
+        },
+        {
+            src: "hda",
+            icon: "fa-files-o",
+            tooltip: "Multiple datasets",
+            multiple: true,
+            batch: Batch.ENABLED,
+        },
+    ],
+    module_data_collection: [
+        {
+            src: "hdca",
+            icon: "fa-folder-o",
+            tooltip: "Dataset collection",
+            multiple: false,
+            batch: Batch.DISABLED,
+        },
+        {
+            src: "hdca",
+            icon: "fa-folder",
+            tooltip: "Multiple collections",
+            multiple: true,
+            batch: Batch.ENABLED,
+        },
+    ],
+};

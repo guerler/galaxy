69351cd776195f625d12931f6fc4b2e2df3f3f2f
guerler <aysam.guerler@gmail.com>
2024-05-03 13:31:43 +0300

    Add failed invocation count to tooltip, only to be shown to owner

--- client/src/components/Workflow/WorkflowInvocationsCount.vue
+++ client/src/components/Workflow/WorkflowInvocationsCount.vue
@@ -2,9 +2,10 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 import { invocationCountsFetcher } from "@/api/workflows";
+import { useUserStore } from "@/stores/userStore";
 import localize from "@/utils/localization";
 
 library.add(faList);
@@ -13,12 +14,27 @@ interface Props {
     workflow: any;
 }
 
+const userStore = useUserStore();
+
 const props = defineProps<Props>();
 
-const count = ref<number | undefined>(undefined);
+const count = ref<number>(0);
+const failed = ref<number>(0);
+const loaded = ref<boolean>(false);
+
+const tooltipWithCount = computed(() => {
+    const tooltip = "View workflow invocations";
+    const ownedByUser = props.workflow.owner === userStore.currentUser?.username;
+    if (ownedByUser && failed.value > 0) {
+        return localize(`${tooltip} (${failed.value} runs failed)`);
+    }
+    return localize(tooltip);
+});
 
 async function initCounts() {
     const { data } = await invocationCountsFetcher({ workflow_id: props.workflow.id });
+    loaded.value = true;
+    console.log(data);
     let allCounts = 0;
     for (const stateCount of Object.values(data)) {
         if (stateCount) {
@@ -26,6 +42,9 @@ async function initCounts() {
         }
     }
     count.value = allCounts;
+    if (data.failed) {
+        failed.value = data.failed;
+    }
 }
 
 onMounted(initCounts);
@@ -33,14 +52,14 @@ onMounted(initCounts);
 
 <template>
     <div class="workflow-invocations-count d-flex align-items-center flex-gapx-1">
-        <BBadge v-if="count != undefined && count === 0" pill class="list-view">
+        <BBadge v-if="loaded && count === 0" pill class="list-view">
             <span>never run</span>
         </BBadge>
         <BBadge
-            v-else-if="count != undefined && count > 0"
+            v-else-if="loaded && count > 0"
             v-b-tooltip.hover.noninteractive
             pill
-            :title="localize('View workflow invocations')"
+            :title="tooltipWithCount"
             class="outline-badge cursor-pointer list-view"
             :to="`/workflows/${props.workflow.id}/invocations`">
             <FontAwesomeIcon :icon="faList" fixed-width />
